73acfed48c2d81a1ff8adb3f2c18df7f8444a45b
salaverryyy <david.salaverry@utec.edu.pe>
2025-05-22 23:24:25 -0500

    Iteratorr

--- AVL_Iterator.h
+++ AVL_Iterator.h
@@ -1,23 +1,61 @@
 #include "AVL_Node.h"
 
-/*Forward Iterator for the AVL*/
 template <typename T>
-class AVLIterator
-{
+class AVLIterator {
 public:
     enum Type {
         PreOrder, InOrder, PostOrder, BFS
     };
+
 private:
-    NodeAVL<T> *current;
+    NodeAVL<T>* current;
     Type type;
-public:
-    AVLIterator() : current(nullptr), type(InOrder) {};
-    AVLIterator(NodeAVL<T> *current, Type type=InOrder): type(type) {
-        //TODO
+    NodeAVL<T>* root;
+    NodeAVL<T>* nextNode;
+
+    void findNextInOrder(NodeAVL<T>* node) {
+        if (node == nullptr) return;
+
+        if (node->left != nullptr) {
+            current = node->left;
+            return;
+        }
+
+        current = node;
     }
-    AVLIterator<T> &operator=(AVLIterator<T> other);
-    bool operator!=(AVLIterator<T> other);
-    AVLIterator<T> &operator++();   //++it
-    T operator*();
+
+public:
+    AVLIterator() : current(nullptr), type(InOrder), root(nullptr), nextNode(nullptr) {}
+
+    AVLIterator(NodeAVL<T>* root, Type type = InOrder) : root(root), type(type), current(nullptr) {
+        if (root != nullptr) {
+            if (type == InOrder) {
+                findNextInOrder(root);
+            }
+        }
+    }
+
+
+    AVLIterator<T>& operator=(AVLIterator<T> other) {
+        std::swap(current, other.current);
+        std::swap(type, other.type);
+        return *this;
+    }
+
+    bool operator!=(AVLIterator<T> other) {
+        return current != other.current;
+    }
+
+    AVLIterator<T>& operator++() {
+        if (type == InOrder) {
+            if (current != nullptr) {
+                findNextInOrder(current->right);
+            }
+        }
+        return *this;
+    }
+
+    T operator*() {
+        return current->data;
+    }
 };
